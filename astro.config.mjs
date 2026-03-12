f53c713586aa8e2361bdc16edba8ff006bcf91e6
All-Vice <omnivice3@gmail.com>
2026-03-11 21:54:55 -0400

    fix: configure @astrojs/tailwind with applyBaseStyles: false

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,9 @@ export default defineConfig({
   site: 'https://all-vice.github.io',
   base: '/',
   integrations: [
-    tailwind(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
     mdx()
   ],
   markdown: {
